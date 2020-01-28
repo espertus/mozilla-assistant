ed799a29a6ce4d86acc22e9910042e9634b2a915
Ellen Spertus <ellen.spertus@gmail.com>
2020-01-28 11:57:33 -0800

    Eliminate roboelectric build warnings

--- app/src/test/java/mozilla/voice/assistant/intents/AlarmIntentMatcherTest.kt
+++ app/src/test/java/mozilla/voice/assistant/intents/AlarmIntentMatcherTest.kt
@@ -1,31 +1,51 @@
 package mozilla.voice.assistant.intents
 
 import android.provider.AlarmClock
-import mozilla.voice.assistant.intents.AlarmIntentMatcher
+import mozilla.voice.assistant.IntentMatcherResult
 import org.junit.Assert.assertEquals
-import org.junit.Assert.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
-@Config(manifest= Config.NONE)
+@Config(manifest = Config.NONE)
 class AlarmIntentMatcherTest {
     private val matcher = AlarmIntentMatcher()
+
+    private fun getScoreHourMinute(result: IntentMatcherResult): Triple<Double, Int, Int> =
+        Triple(
+            result.score,
+            result.intent.getIntExtra(AlarmClock.EXTRA_HOUR, DEFAULT_INT_VALUE),
+            result.intent.getIntExtra(AlarmClock.EXTRA_MINUTES, DEFAULT_INT_VALUE)
+        )
+
+    private fun matchScoreHourMinute(
+        result: IntentMatcherResult,
+        score: Double,
+        hour: Int,
+        minutes: Int
+    ) {
+        val (s, h, m) = getScoreHourMinute(result)
+        assertEquals(score, s, DELTA)
+        assertEquals(hour, h)
+        assertEquals(minutes, m)
+    }
 
     @Test
     fun matches3PM() {
         val results = matcher.matchTranscript("set alarm for 3 p.m.")
         assertEquals(1, results.size)
-        val result = results[0]
-        assertEquals(AlarmIntentMatcher.CONFIDENCE_WITH_HP, result.score, DELTA)
-        val intent = result.intent
-        assertEquals(15, intent.getIntExtra(AlarmClock.EXTRA_HOUR, -1))
-        assertEquals(0, intent.getIntExtra(AlarmClock.EXTRA_MINUTES, -1))
+        matchScoreHourMinute(
+            results[0],
+            AlarmIntentMatcher.CONFIDENCE_WITH_HP,
+            15,
+            0
+        )
     }
 
     companion object {
         private const val DELTA = .01
+        private const val DEFAULT_INT_VALUE = -1 // default argument to Intent.getIntExtra
     }
 }
